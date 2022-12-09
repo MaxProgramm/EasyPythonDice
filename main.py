407375d840e7771926365479976278ee715783d1
schueler <schueler@MacBook-Air-von-ms01.local>
2022-12-09 16:08:50 +0100

    v1.0.0

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+
+wantedInt = int(input("Geben Sie Ihre gewünschte Zahl ein. Bitte nicht ihre IP!"))
+
+
+row1 = ["O", "O", "O"]
+row2 = ["O", "O", "O"]
+row3 = ["O", "O", "O"]
+
+def peter(randInt):
+
+    if randInt == 1:
+        row1 = ["O", "O", "O"]
+        row2 = ["O", "X", "O"]
+        row3 = ["O", "O", "O"]
+    if randInt == 2:
+        row1 = ["X", "O", "O"]
+        row2 = ["O", "O", "O"]
+        row3 = ["O", "O", "X"]
+    if randInt == 3:
+        row1 = ["X", "O", "O"]
+        row2 = ["O", "X", "O"]
+        row3 = ["O", "O", "X"]
+    if randInt == 4:
+        row1 = ["X", "O", "X"]
+        row2 = ["O", "O", "O"]
+        row3 = ["X", "O", "X"]
+    if randInt == 5:
+        row1 = ["X", "O", "X"]
+        row2 = ["O", "X", "O"]
+        row3 = ["X", "O", "X"]
+    if randInt == 6:
+        row1 = ["X", "O", "X"]
+        row2 = ["X", "O", "X"]
+        row3 = ["X", "O", "X"]
+
+    return (row1, row2, row3)
+
+RandomInt = random.randrange(1, 7)
+
+while True:
+    RandomInt = random.randrange(1, 7)
+    for row in peter(RandomInt):
+        print(row)
+    print("------------------------------------")
+    if RandomInt == wantedInt:
+        break
